d673b0837c0a78be49efbd866311b65be7d036bc
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2021-10-22 10:55:40 -0600

    Removed unused code

--- app/src/main/java/com/example/simplecarcreator/MainActivity.java
+++ app/src/main/java/com/example/simplecarcreator/MainActivity.java
@@ -37,27 +37,4 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(car.getString());
         output.setText(car.getString());
     }
-
-/*    public void onHondaInteract(View view) {
-
-        Car car = new Car();
-
-        EditText carModel = (EditText) findViewById(R.id.modelName);
-        String carName = carModel.getText().toString();
-        car.setModelName(carName);
-
-        EditText totalCarDoors = (EditText) findViewById(R.id.totalCarDoors);
-        int carDoors = Integer.parseInt(totalCarDoors.getText().toString());
-        car.setDoors(carDoors);
-
-        EditText milesPerGallon = (EditText) findViewById(R.id.milesPerGallon);
-        float mpg = Float.parseFloat(milesPerGallon.getText().toString());
-        car.setMilesPerGallon(mpg);
-
-        car.createCar(carName, carDoors, mpg);
-
-        TextView output = (TextView) findViewById(R.id.txtOutput);
-        System.out.println(car.getString());
-        output.setText(car.getString());
-    }*/
 }
